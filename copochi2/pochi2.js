742e2e23a69bda6e1973ff2fe23908aca882f920
Tomoki YAMASHITA <tomorrowkey@TomokiYAMASHITA-no-MacBook-Pro.local>
2011-11-21 20:50:27 +0900

    リファクタリング

--- copochi2/pochi2.js
+++ copochi2/pochi2.js
@@ -1,16 +1,21 @@
-// alert('1');
-var archiveButtons = document.getElementsByName('archiveButton');
-if (0 < archiveButtons.length) {
-	// alert('2');
+function performClick(button) {
 	var myevent = document.createEvent('MouseEvents');
-	myevent.initEvent('click', false, true);	
-	archiveButtons[0].dispatchEvent(myevent);
-} else {
+	myevent.initEvent('click', false, true);
+	button.dispatchEvent(myevent);
+}
+
+function autoArchive() {
+	var archiveButtons = document.getElementsByName('archiveButton');
+	if(0 < archiveButtons.length) {
+		performClick(archiveButtons[0]);
+		return;
+	}
+
 	var cancelButtons = document.getElementsByName('canceledOrderButtonCCDeclined');
-	if (0 < cancelButtons.length) {
-		var myevent = document.createEvent('MouseEvents');
-		myevent.initEvent('click', false, true);	
-		cancelButtons[0].dispatchEvent(myevent);
+	if(0 < cancelButtons.length) {
+		performClick(cancelButtons[0]);
+		return;
 	}
 }
-// alert('3');
+
+autoArchive();
